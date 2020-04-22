9b33992cb65b5351fc01b23a66c9741b47ab4818
Cancelkam <Cancel.kam@gmail.com>
2020-04-22 12:24:10 +0300

    tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,11 +7,29 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void remainFrom1000() {
         CashbackHackService cashbackHackService  = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain (amount);
         int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void remainFrom1900() {
+        CashbackHackService cashbackHackService  = new CashbackHackService();
+        int amount = 1900;
+        int actual = cashbackHackService.remain (amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void remainFrom2500() {
+        CashbackHackService cashbackHackService  = new CashbackHackService();
+        int amount = 2500;
+        int actual = cashbackHackService.remain (amount);
+        int expected = 500;
         assertEquals(actual,expected);
     }
 }
