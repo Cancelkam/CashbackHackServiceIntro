5775e26faf2522d9eab7cdd5be8b90bbe51f42e4
Cancelkam <Cancel.kam@gmail.com>
2020-04-23 18:26:52 +0300

    new logic+test added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,28 +8,39 @@ public class CashbackHackServiceTest {
 
     @Test
     public void remainFrom1000() {
-        CashbackHackService cashbackHackService  = new CashbackHackService();
+        CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
-        int actual = cashbackHackService.remain (amount);
+        int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void remainFrom1900() {
-        CashbackHackService cashbackHackService  = new CashbackHackService();
+        CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1900;
-        int actual = cashbackHackService.remain (amount);
+        int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void remainFrom2500() {
-        CashbackHackService cashbackHackService  = new CashbackHackService();
+        CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 2500;
-        int actual = cashbackHackService.remain (amount);
+        int actual = cashbackHackService.remain(amount);
         int expected = 500;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainFromMinus100() throws IllegalArgumentException {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = -100;
+        try {
+            cashbackHackService.remain(amount);
+        } catch (IllegalArgumentException thrown) {
+            assertEquals("amount must be greater than zero", thrown.getMessage());
+        }
     }
 }
